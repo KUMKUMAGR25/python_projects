7ffef776bdf9fdf8a25fa708781e8f8870c1fcf4
Kumkum Agrawal23 <kumkumagr24@gmail.com>
2026-08-14 23:28:40 +0530

    add grade calculator

---
+++ Grade.calculator.py
@@ -0,0 +1,25 @@
+name = input("Enter student name: ")
+
+sub1 = float(input("Enter marks for Subject 1: "))
+sub2 = float(input("Enter marks for Subject 2: "))
+sub3 = float(input("Enter marks for Subject 3: "))
+
+total = sub1 + sub2 + sub3
+percentage = total / 3
+
+print("Student Name:", name)
+print("Total Marks:", total)
+print("Percentage:", percentage)
+
+if percentage >= 90:
+    print("Grade: A+")
+elif percentage >= 80:
+    print("Grade: A")
+elif percentage >= 70:
+    print("Grade: B")
+elif percentage >= 60:
+    print("Grade: C")
+elif percentage >= 50:
+    print("Grade: D")
+else:
+    print("Grade: F")
